f7c628743e3b5df4f3ade9b4213564337d169199
IToriginal <gitshineit@gmail.com>
2023-09-30 21:04:46 +0900

    🎉 Starting Project

---
+++ src/main/java/com/itoriginal/generalprojectboard/GeneralProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.itoriginal.generalprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itoriginal/generalprojectboard/GeneralProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itoriginal.generalprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
